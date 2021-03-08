107917cf3b4a0adacec9b16839362f8b008af3f5
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-03-08 12:51:16 -0700

    added notes

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
+//loading the modules
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+//use morgan to log common info
 app.use(morgan("common"));
 
+//get /movies
 app.get("/movies", (req, res) => {
   res.json(topMovies);
 });
 
+//load a generic message 
 app.get("/", (req, res) => {
   res.send("Welcome to my MovieFlix App");
 });
 
+//summon express staic on public 
 app.use(express.static("public"));
 
+//access documentation using express static
 app.get("/documentation", (req, res) => {
   res.sendFile("public/documentation.html", { root: _dirname });
 });
 
+//error handling
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
 });
 
+// log listening on 8080
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080");
 });
