c9dc53f3f14db1d9a15098eaf3ce4ba9c03455a5
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-03-08 11:04:27 -0700

    updating dependencies

--- index.js
+++ index.js
@@ -1,34 +1,50 @@
-
-const express = require('express');
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require("express");
 const app = express();
+morgan = require('morgan');
+const http = require("http");
+url = require("url");
 
-const http = require('http');
+let requestTime = (req, res, next) => {
+  req.requestTime = Date.now();
+  next();
+};
 
-url = require('url');
+app.use(morgan('common'));
+app.use(requestTime);
 
+app.get("/", (req, res) => {
+  let responseText = "Welcome to my Movie app!";
+  responseText += "<small>Requested at: " + req.requestTime + "</small>";
+  res.send(responseText);
+});
 
-let topMovies= [
-  {title: 'Bladerunner',
-  starring: 'Harrison Ford'},
-  {title: 'Alien',
-  starring: 'Sigourney Weaver'},
-  {title: 'The Chronical Of Riddick',
-  starring: 'Vin Diesel'}  
+app.get("/secreturl", (req, res) => {
+  res.send("This is a secret url with super top-secret content.");
+});
+
+let topMovies = [
+  { title: "Bladerunner", starring: "Harrison Ford" },
+  { title: "Alien", starring: "Sigourney Weaver" },
+  { title: "The Chronical Of Riddick", starring: "Vin Diesel" },
 ];
 
 //get requests
-app.get('/', (req, res) => {
-    res.send('Welcome to my Movieclub');
+app.get("/", (req, res) => {
+  res.send("Welcome to my Movieclub");
 });
 
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', {root: _dirname});
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: _dirname });
 });
 
-app.get('/movies', (req, res) => {
-    res.json(topMovies);
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
 });
 
 app.listen(8080, () => {
-    console.log('Your app is listening on port 8080');
+  console.log("Your app is listening on port 8080");
 });
