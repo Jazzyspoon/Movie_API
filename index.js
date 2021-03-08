6ba27d7b7961635fd9081606c0ac00b418ab7ebb
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-03-08 11:45:52 -0700

    removing eslint and updating folder structure

--- index.js
+++ index.js
@@ -1,50 +1,35 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const express = require("express");
 const app = express();
 morgan = require('morgan');
 const http = require("http");
 url = require("url");
 
-let requestTime = (req, res, next) => {
-  req.requestTime = Date.now();
-  next();
-};
-
 app.use(morgan('common'));
-app.use(requestTime);
+app.use(express.static('public'));
+
+app.get("/movies", (req, res) => {
+    res.json(topMovies);
+  });
 
 app.get("/", (req, res) => {
-  let responseText = "Welcome to my Movie app!";
-  responseText += "<small>Requested at: " + req.requestTime + "</small>";
-  res.send(responseText);
-});
-
-app.get("/secreturl", (req, res) => {
-  res.send("This is a secret url with super top-secret content.");
-});
-
-let topMovies = [
-  { title: "Bladerunner", starring: "Harrison Ford" },
-  { title: "Alien", starring: "Sigourney Weaver" },
-  { title: "The Chronical Of Riddick", starring: "Vin Diesel" },
-];
-
-//get requests
-app.get("/", (req, res) => {
-  res.send("Welcome to my Movieclub");
+  res.send("Welcome to my MovieFlix App");
 });
 
 app.get("/documentation", (req, res) => {
-  res.sendFile("public/documentation.html", { root: _dirname });
+  res.sendFile(express.static('public/documentation.html', { root: _dirname }));
 });
 
-app.get("/movies", (req, res) => {
-  res.json(topMovies);
-});
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
 
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080");
 });
+
+// let topMovies = [
+//     { title: "Bladerunner", starring: "Harrison Ford" },
+//     { title: "Alien", starring: "Sigourney Weaver" },
+//     { title: "The Chronical Of Riddick", starring: "Vin Diesel" },
+//   ];
