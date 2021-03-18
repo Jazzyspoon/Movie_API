2ad42f0a99973f2ec89faba0a616e851f06b9d44
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-03-18 16:18:21 -0600

    Update models.js

--- models.js
+++ models.js
@@ -5,25 +5,25 @@ let movieSchema = mongoose.Schema({
   Description: { type: String, required: true },
   Genre: {
     Name: String,
-    Description: String
+    Description: String,
   },
   Director: {
     Name: String,
     Bio: String,
     Birth: String,
-    Death: String
+    Death: String,
   },
   Actors: [String],
   ImagePath: String,
-  Featured: Boolean
+  Featured: Boolean,
 });
 
 let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
+  Username: { type: String, required: true },
+  Password: { type: String, required: true },
+  Email: { type: String, required: true },
   Birthday: Date,
-  Favoritemovies: [{type: mongoose.Schema.Types.ObjectId, ref:"Movie"}]
+  Favoritemovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }],
 });
 
 let Movie = mongoose.model("Movie", movieSchema);
