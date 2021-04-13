4c37560018e348991f702e55f060b6a63fa18df7
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-04-13 09:03:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -17,23 +17,23 @@ const app = express();
 app.use(express.json());
 app.use(bodyParser.json());
 let auth = require("./auth")(app);
-
+app.use(cors());
 //cors security
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         // If a specific origin isn’t found on the list of allowed origins
+//         let message =
+//           "The CORS policy for this application doesn’t allow access from origin " +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 //summon express static on public
 app.use(express.static("public"));
